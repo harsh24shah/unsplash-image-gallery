d45d6091937b435a2bf43bd65584d95bc34c827b
username <harsh.120410116038@gmail.com>
2019-11-19 13:21:38 +0530

    refractor code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyBIRCn5qfgOmkL3G0fiEw2xSL8xNgdjam0',
-    authDomain: 'wallperex.firebaseapp.com',
-    databaseURL: 'https://wallperex.firebaseio.com',
-    projectId: 'wallperex',
-    storageBucket: 'wallperex.appspot.com',
-    messagingSenderId: '526743207581'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyBIRCn5qfgOmkL3G0fiEw2xSL8xNgdjam0',
-    authDomain: 'wallperex.firebaseapp.com',
-    databaseURL: 'https://wallperex.firebaseio.com',
-    projectId: 'wallperex',
-    storageBucket: 'wallperex.appspot.com',
-    messagingSenderId: '526743207581'
-  }
+  production: false
 };
 
 /*
